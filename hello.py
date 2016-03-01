057109d73a6992aed5f2905ca611135a2fb905e1
isma91 <ismaydogmus@gmail.com>
2016-03-01 16:11:37 +0100

    New release 2.0 for hello.py

--- hello.py
+++ hello.py
@@ -1,28 +1,43 @@
 #!/usr/bin/python
-import sys
-def afficher(nom, prenom, age):
-	print("Hello my name is ", nom, prenom, " and i'm " , age , " years old !!");
-	return
-error = 0;
-if len(sys.argv) == 2 and sys.argv[1] == "--help":
-	print("Help to use indx.py !!\n");
-	print("hello.py [--lastname] [--firstname] [--age]\n");
-	print("--lastname=Foo	Your lastname\n");
-	print("--firstname=Bar	Your firstname\n");
-	print("--age=42	Your age\n");
-elif len(sys.argv) != 4:
-	error = error + 1
-	print("You must write your lastname, firstname and age !!\n");
-	print("Type index.py --help to have some info !!\n");
-elif len(sys.argv) == 4:
-	if str(sys.argv[1]) == "" or sys.argv[1][11:] == "":
-		error = error + 1;
-		print("Your lastname can't be empty !!");
-	if str(sys.argv[2]) == "" or sys.argv[2][12:] == "":
-		error = error + 1;
-		print("Your firstname can't be empty !!");
-	if str(sys.argv[3]) == "" or sys.argv[3][6:] == "":
-		error = error + 1;
-		print("Your age can't be empty !!");
-if error == 0:
-	afficher(sys.argv[1][11:], sys.argv[2][12:], sys.argv[3][6:]);
+def oneself():
+    lastname = input("Your Lastname ?\n");
+    lastname = lastname.strip();
+
+    while lastname == "":
+        lastname = input("Your Lastname ?\n");
+        lastname = lastname.strip();
+
+    firstname = input("Your Firstname ?\n");
+    firstname = firstname.strip();
+
+    while firstname == "":
+        firstname = input("Your Firstname ?\n");
+        firstname = firstname.strip();
+
+    age = input("How old are you ?\n");
+    age = age.strip();
+
+    while age.isdigit() == False:
+        age = input("How old are you ?\n");
+        age = age.strip();
+
+    print("Hello", lastname, firstname, "!! You're", age, "years old !!\n");
+    print("Thanks you !!");
+    return;
+def hello_help():
+    print("This script gonna ask you your Firstname, Lastname and your age\n");
+    print("If you answer an empty answer, he will ask you again\n");
+    print("If you answer other than an integer for your age, he will ask you again\n");
+    print("Thanks you !!\n");
+    check_answer();
+    return;
+def check_answer():
+    answer = input("Welcome to Hello !!! Did you need help or did you want to present yourself ?\nTape help or present : ");
+    while answer != "help" and answer != "present":
+        answer = input("Welcome to Hello !!! Did you need help or did you want to present yourself ?\nTape help or present : ");
+    if answer == "help":
+        hello_help();
+    elif answer == "present":
+        oneself();
+    return;
+check_answer();
